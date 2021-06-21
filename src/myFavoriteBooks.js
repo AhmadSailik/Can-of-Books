c6839bec1019b52b09081b7a5e6512e7056bd68b
AhmadSailik <asailik1993@gmail.com>
2021-06-21 22:40:30 +0300

    add bestbook

--- src/myFavoriteBooks.js
+++ src/myFavoriteBooks.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import BestBooks from './BestBooks';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './myFavoriteBooks.css';
 
 
 class MyFavoriteBooks extends React.Component {
-  render() {
-    
+
+ 
+ 
+
+  
+  render (){
+  
     return (
-      <Jumbotron>
+      <Jumbotron  >
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
         </p>
+    <BestBooks/>
       </Jumbotron>
+      
     )
   }
 }
